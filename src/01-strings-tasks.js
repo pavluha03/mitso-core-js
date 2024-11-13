5fafe23210f62e8674cb7e6078955741c2a44610
pavluha03 <pavluha0411@gmail.com>
2024-11-13 18:39:54 +0300

    01-5

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -85,8 +85,8 @@ function extractNameFromTemplate(value) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value[0];
 }
 
 /**
